29f14769ccffccf33ad5fb28f55711b34261df6b
Jenny Wang <219030445+CnertaNureta@users.noreply.github.com>
2026-05-04 00:02:01 +0800

    Increase static page generation timeout

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,7 @@ const withSerwist = withSerwistInit({
 })
 
 const nextConfig: NextConfig = {
+  staticPageGenerationTimeout: 180,
   images: {
     deviceSizes: [390, 640, 828, 1080, 1200, 1920],
     formats: ['image/avif', 'image/webp'],
